db0b0ccc7b0944b0fe50ddd00c9f35ba1bf93e8b
Ubuntu <ubuntu@ip-172-31-21-90.ap-northeast-1.compute.internal>
2025-01-09 00:35:10 +0800

    feature: Add mission bot intro control

--- bot/constants.py
+++
@@ -1,8 +0,0 @@
-import json
-
-TASK_MEDAL = {}
-
-with open("bot/data/mission_transcriptions.json", "r") as file:
-    transcriptions = json.load(file)
-
-MISSIONS = {obj['mission_id']: obj for obj in transcriptions}
