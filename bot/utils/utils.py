bf8dc3166c824999a9a2158579ea0b5bd8cb446c
Ubuntu <ubuntu@ip-172-31-21-90.ap-northeast-1.compute.internal>
2025-01-09 00:39:02 +0800

    feature: api corresponding update

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -5,9 +5,42 @@ import requests
 import aiohttp
 import discord
 
-from bot.constants import TASK_MEDAL
 from bot.config import config
 
+async def fetch_student_list():
+    api_url = f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_student_list'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return data.get("data", [])
+            else:
+                return []
+
+async def get_mission_info(mission_id):
+    api_url = f"http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_mission_list"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url) as response:
+            data = await response.json()
+            return data['data'][int(mission_id)-1]
+
+async def update_mission_assistant(mission_id, assistant_id):
+    #user_id = str(user_id)
+    api_url = f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/update_mission_assistant'
+    data = {
+        'mission_id': int(mission_id),
+        'assistant_id': assistant_id
+    }
+
+    try:
+        response = requests.post(
+            api_url,
+            json=data,
+        )
+        data = response.json()
+        logger.debug(f'Update mission assistant API response: {data}')
+    except Exception as e:
+        logger.error(f'Update mission assistant API Error: {e}')
 
 async def fetch_baby_list():
     api_url = f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_baby_list'
@@ -16,13 +49,11 @@ async def fetch_baby_list():
             return await response.json()
 
 
-async def send_task_unfinished_notification(client, discord_id, last_task_title):
+async def send_task_unfinished_notification(client, discord_id, mission):
     user = await client.fetch_user(discord_id)
-
     await user.send(
-        f'嗨，您好嗎？我知道您忙著照顧寶寶，"{last_task_title}"的諮詢還未結束，若您有空時請隨意回覆，我會隨時在這裡為您服務。'
+        f'嗨，您好嗎？我知道您忙著照顧寶寶，任務還沒結束喔，若您有空上課時，請呼叫加一，我會隨時在這裡為您服務。'
     )
-
 
 async def update_student_mission_status(user_id, mission_id, total_steps=0, current_step=0, thread_id=None, assistant_id=None):
     # thread_id is none only if the status is complete
@@ -80,39 +111,48 @@ async def job(client):
     if channel is None or not isinstance(channel, discord.TextChannel):
         raise Exception('Invalid channel')
 
-    data = await get_student_class_notification()
+    data = await get_student_mission_notification_list()
 
     tasks_to_be_pushed = data.get('notification_list', [])
-    tasks_to_be_notified = data.get('incomplete_class_reminder_list', [])
+    tasks_to_be_notified = data.get('incomplete_mission_reminder_list', [])
 
     # Push tasks to students
-    for task in tasks_to_be_pushed:
-        discord_id = task['discord_id']
-        task_id = task['registered_class_id']
+    for mission in tasks_to_be_pushed:
+        discord_id = mission['discord_id']
+        mission_id = mission['mission_id']
 
         # Start task
-        await channel.send(f'START_TASK_{task_id} <@{discord_id}>')
+        await channel.send(f'START_MISSION_{mission_id} <@{discord_id}>')
 
     # Notify the user if they haven't completed the previous task
-    for task in tasks_to_be_notified:
-        discord_id = task['discord_id']
-        last_task_title = task['class_title']
-        await send_task_unfinished_notification(client, discord_id, last_task_title)
+    for mission in tasks_to_be_notified:
+        discord_id = mission['discord_id']
+        last_mission_title = task['mission_title']
+        await send_task_unfinished_notification(client, discord_id, last_mission_title)
 
-
-async def get_student_class_notification():
-    api_url = f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_student_class_notification_list'
+async def get_student_mission_notification_list():
+    api_url = f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_student_mission_notification_list'
     try:
         response = requests.get(api_url)
-        data = response.json()
+        data = response.json()['']
     except Exception as e:
-        logger.error(f'get_student_class_notification Error: {e}')
-        data = {}
+        logger.error(f'get_student_mission_notification_list Error: {e}')
+        data = []
 
     return data
 
+async def get_student_incompleted_mission_list(user_id):
+    api_url = f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_student_incompleted_mission_list'
+    try:
+        response = requests.post(api_url, json={'discord_id': str(user_id)})
+        data = response.json()['data']
+        logger.debug(f'get_student_incompleted_missions API response: {data}')
+    except Exception as e:
+        logger.error(f'get_student_incompleted_missions Error: {e}')
+        data = []
+    return data
 
-async def get_baby_record(discord_id):
+async def get_baby_profile(discord_id):
     try:
         baby_list = (await fetch_baby_list())['data']
     except Exception as e:
@@ -130,16 +170,6 @@ async def get_baby_record(discord_id):
     day_age = (datetime.today().date() - birth_date).days
     baby_name = baby['baby_name']
     baby_gender = '男' if baby['gender'] == 'm' else '女'
-
-    api_url = (
-        f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_baby_records'
-    )
-    try:
-        response = requests.post(api_url, json={'discord_id': str(discord_id)})
-        data = response.json()
-    except Exception as e:
-        logger.error(f'Error: {e}')
-        data = {'records': []}
 
     record_str = f'今天日期: {datetime.today().date()}'
     record_str += f'\n寶寶姓名: {baby_name}'
@@ -177,6 +207,33 @@ async def get_baby_record(discord_id):
 
     return record_str
 
+async def get_baby_record(discord_id):
+    api_url = (
+        f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/get_baby_records'
+    )
+    try:
+        response = requests.post(api_url, json={'discord_id': str(discord_id)})
+        data = response.json()
+    except Exception as e:
+        logger.error(f'Error: {e}')
+        data = {'records': []}
+
+    records = data.get('records', [])
+
+    # check records in the last 2 weeks
+    records = [
+        record
+        for record in records
+        if (
+            datetime.today() - datetime.strptime(record['timestamp'], '%Y-%m-%d %H:%M')
+        ).days
+        <= 14
+    ]
+
+    if len(records) > 0:
+        return True
+    else:
+        return False
 
 async def store_message(user_id, role, timestamp, message):
     api_url = f'http://{config.BABY_API_HOST}:{config.BABY_API_PORT}/api/add_student_chat_data'
