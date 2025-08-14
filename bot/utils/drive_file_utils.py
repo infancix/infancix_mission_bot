33ecc1631c92865402db75baf15634b786ebb07f
HsinPingPing <k26021409@gmail.com>
2025-08-14 10:40:51 +0800

    fix: drive_file_utils.py

--- bot/utils/drive_file_utils.py
+++ bot/utils/drive_file_utils.py
@@ -24,29 +24,22 @@ def get_google_drive_download_url(file_id):
 def get_google_drive_preview_image_url(file_id):
     return f"https://drive.google.com/uc?export=view&id={file_id}"
 
-async def create_file_from_url(url, filename="image.png", cache_dir="cache"):
+async def create_file_from_url(url, cache_dir="cache"):
     file_id = extract_google_drive_file_id(url)
     if not file_id:
         return None
     
     cache_path = Path(cache_dir)
     cache_path.mkdir(exist_ok=True)
-    
-    file_extension = Path(filename).suffix
-    cached_file_path = cache_path / f"{file_id}{file_extension}"
-    
+    cached_file_path = cache_path / f"{file_id}.png"
     if cached_file_path.exists():
-        return discord.File(cached_file_path, filename=filename)
-    
+        return discord.File(cached_file_path, filename=cached_file_path.name)
+
     async with aiohttp.ClientSession() as session:
         async with session.get(get_google_drive_download_url(file_id)) as response:
             if response.status == 200:
                 data = await response.read()
-                
                 with open(cached_file_path, 'wb') as f:
                     f.write(data)
-                
-                return discord.File(BytesIO(data), filename=filename)
-    
+                return discord.File(BytesIO(data), filename=cached_file_path.name)
     return None
-
